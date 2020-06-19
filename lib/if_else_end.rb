437fdfc932da87fec0009ad0d906c7292a0ea8b4
Soroosh Golbabae <soroosh.gb@gmail.com>
2020-06-19 21:18:59 +0000

    Done.

--- lib/if_else_end.rb
+++ lib/if_else_end.rb
@@ -1,1 +1,14 @@
-# Write your solution here
+
+if Time.now.to_i % 2 == 0
+  puts "Even!"
+else
+  puts "Odd!"
+end
+
+current_time = Time.now
+current_time = current_time.to_i
+if current_time.even?
+  puts "Even!"
+else
+  puts "Odd!"
+end
---
+++ my_test.rb
@@ -0,0 +1,7 @@
+if chance_of_rain <= 0.25
+  puts "pack a sun shelter!"
+ elsif (chance_of_rain > 0.25 && chance_of_rain < 0.75)
+   puts "Pack an umbrella!"
+ else
+   puts "Stay home and read Hegel."
+ end
